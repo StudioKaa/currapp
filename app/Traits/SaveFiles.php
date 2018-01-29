d9913678f622903c327da967eaa5973f39e1e3c5
Bart Roos <bart@studiokaa.co>
2018-01-29 14:41:18 +0100

    Change filenames

--- app/Traits/SaveFiles.php
+++ app/Traits/SaveFiles.php
@@ -8,17 +8,19 @@ trait SaveFiles{
     {
         //build general filename
         $filename = date('Ymd');
+        
+        $filename .= 'Reader_' . $review->lesson->lesson_type->term->title;
         $filename .= '_' . $review->lesson->lesson_type->title;
-        $filename .= '_' . $review->lesson->lesson_type->term->title;
-        $filename .= '_' . $review->lesson->week_start;
+        $filename .= '_wk' . $review->lesson->week_start;
         $filename .= '_' . $review->lesson->getFileName();
+        $filename .= '_rev' . $review->id;
         
         //save wv
         if($request->hasFile('wv_file'))
         {
             $wv_ext = request()->wv_file->getClientOriginalExtension();
             $review->wv_filename = $filename . '.' . $wv_ext;
-            $wv_do = 'review_' . $review->id . '_wv.' . $wv_ext;
+            $wv_do = $review->wv_filename;
             $review->wv_do_path = Storage::disk('spaces')->putFileAs('uploads/reviews', request()->wv_file, $wv_do, 'private');
         }
 
@@ -27,7 +29,7 @@ trait SaveFiles{
         {
             $tv_ext = request()->tv_file->getClientOriginalExtension();
             $review->tv_filename = $filename . '_TV.' . $tv_ext;
-            $tv_do = 'review_' . $review->id . '_tv.' . $tv_ext;
+            $tv_do = $review->tv_filename;
             $review->tv_do_path = Storage::disk('spaces')->putFileAs('uploads/reviews', request()->tv_file, $tv_do, 'private');
         }
 
@@ -36,7 +38,7 @@ trait SaveFiles{
         {
             $sv_ext = request()->sv_file->getClientOriginalExtension();
             $review->sv_filename = $filename . '_SV.' . $sv_ext;
-            $sv_do = 'review_' . $review->id . '_sv.' . $sv_ext;
+            $sv_do = $review->sv_filename;
             $review->sv_do_path = Storage::disk('spaces')->putFileAs('uploads/reviews', request()->sv_file, $sv_do, 'private');
         }
         
