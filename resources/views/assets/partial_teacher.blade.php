567c1d644315995a2de24ff71e07efe9e0af62db
Bart Roos <bart@studiokaa.co>
2018-02-28 15:43:19 +0100

    Finish partial_teacher for assets

--- resources/views/assets/partial_teacher.blade.php
+++ resources/views/assets/partial_teacher.blade.php
@@ -1,19 +1,18 @@
-    <h5>Assets</h5>
-    <div class="btn-group">
-        <span class="btn btn-outline-secondary">Nieuw:</span>
-        <a class="btn btn-outline-secondary" href="{{ route('lessons.assets.create.file', $lesson->id) }}" class="btn"><i class="fa fa-file-text-o"></i> bestand</a>
-        <a class="btn btn-outline-secondary" href="{{ route('lessons.assets.create.link', $lesson->id) }}" class="btn"><i class="fa fa-link"></i> link</a>
-    </div>
+<div class="btn-group asset-buttons">
+    <span class="btn btn-outline-secondary">Nieuw:</span>
+    <a class="btn btn-outline-secondary" href="{{ route('lessons.assets.create.file', $lesson->id) }}" class="btn"><i class="fa fa-file-text-o"></i> bestand</a>
+    <a class="btn btn-outline-secondary" href="{{ route('lessons.assets.create.link', $lesson->id) }}" class="btn"><i class="fa fa-link"></i> link</a>
+</div>
+
 @if($lesson->assets->isNotEmpty())
 <div class="assets">
     <div class="assets card">
         <ul class="list-group list-group-flush">
             @foreach($lesson->assets as $asset)
                 <li class="list-group-item">
-                    <a target="_blank" href="{{ $asset->link }}" >
-                        {{ $asset->title }}
-                    </a>
-                    <a href="/files/{{ $asset->id }}/delete" class="pull-right"><i class="fa fa-trash"></i></a>
+                    <a target="_blank" href="{{ $asset->link }}">{{ $asset->title }}</a>
+                    <small class="text-muted">- {{ $asset->created_at->diffForHumans() }} door {{ $asset->author()->name }}</small>
+                    <a href="{{ route('lessons.assets.delete', [$lesson, $asset]) }}" class="pull-right"><i class="fa fa-trash"></i></a>
                 </li>
             @endforeach
         </ul>
